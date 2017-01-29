f65db3d064eab9540315a187354ff32daa8b45b5
lionkgb <lionceau.lk@gmail.com>
2017-01-29 23:00:50 +0200

    2nd exercise in oop

--- oop/hotel_reservation.rb
+++ oop/hotel_reservation.rb
@@ -25,6 +25,57 @@ class HotelReservation
   end
 end
 
+reservation = HotelReservation.new({customer_name: "Lion", date:"2017/Jan/29", room_number: "021"})
+puts 'Testing hotel reservation class'
+p reservation
+puts 
+puts 'Testing to change a room number'
+result = reservation.room_number=('012')
+puts 'Your method returned...'
+puts result
+puts 
+if result == '012'
+  puts 'PASS'
+else 
+  puts 'FAIL'
+end
+puts
+puts 'Testing adding fridge method'
+result = reservation.add_a_fridge
+puts 'Your method returned...'
+puts result
+if reservation.amenities.include?("fridge")
+  puts 'PASS'
+else 
+  puts 'FAIL'
+end
+puts 
+puts 'Testing adding cribe method'
+result = reservation.add_a_crib
+puts 'Your method returned...'
+puts result
+if reservation.amenities.include?("crib")
+  puts 'PASS'
+else 
+  puts 'FAIL'
+end
+puts 
+if reservation.amenities.include?("crib")
+  puts 'PASS'
+else 
+  puts 'FAIL'
+end
+puts 
+result = reservation.add_a_custom_amenity('TV')
+puts 'Your method returned...'
+puts result 
+if reservation.amenities.include?('TV')
+  puts 'PASS'
+else
+  puts 'FAIL'
+end
+
+
 
 # Write your own driver code below! Make sure your code tests the following:
 # The ability to change a room number even after a reservation has already been created
